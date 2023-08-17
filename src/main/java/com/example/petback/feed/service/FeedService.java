f1107a23c507ba7e1f6cf3beddfe569ec2534f34
Jongwoo Choi <muvnelik@naver.com>
2023-08-17 16:10:08 +0900

    feat: Feed CRUD 구현
    - ServiceImpl 작성 완료

--- src/main/java/com/example/petback/feed/service/FeedService.java
+++ src/main/java/com/example/petback/feed/service/FeedService.java
@@ -1,8 +1,8 @@
 package com.example.petback.feed.service;
 
-import com.example.petback.common.advice.ApiResponseDto;
 import com.example.petback.feed.dto.FeedRequestDto;
 import com.example.petback.feed.dto.FeedResponseDto;
+import com.example.petback.feed.entity.Feed;
 import com.example.petback.user.entity.User;
 
 import java.util.List;
@@ -10,14 +10,18 @@ import java.util.List;
 public interface FeedService {
 
     //생성
-    ApiResponseDto createFeed(FeedRequestDto requestDto, User user);
+
+    FeedResponseDto createFeed(FeedRequestDto requestDto, User user);
+
     //전체 조회
-    List<FeedResponseDto> searchFeeds();
+    List<FeedResponseDto> selectFeeds();
     //상세 조회
-
-
+    Feed selectFeed(Long id);
     //수정
+    FeedResponseDto updateFeed(Long id, FeedRequestDto requestDto, User user);
+
     //삭제
+    void deleteFeed(Long id, User user);
 
 
 }
--- src/main/java/com/example/petback/feed/service/FeedServiceImpl.java
+++ src/main/java/com/example/petback/feed/service/FeedServiceImpl.java
@@ -1,4 +1,66 @@
 package com.example.petback.feed.service;
 
-public class FeedServiceImpl {
+import com.example.petback.feed.dto.FeedRequestDto;
+import com.example.petback.feed.dto.FeedResponseDto;
+import com.example.petback.feed.entity.Feed;
+import com.example.petback.feed.repository.FeedRepository;
+import com.example.petback.user.entity.User;
+import com.example.petback.user.enums.UserRoleEnum;
+import com.example.petback.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.RejectedExecutionException;
+
+@Service
+@RequiredArgsConstructor
+public class FeedServiceImpl implements FeedService {
+    private final FeedRepository feedRepository;
+    private final UserRepository userRepository;
+
+    //피드 생성
+    @Override
+    public FeedResponseDto createFeed(FeedRequestDto requestDto, User user) {
+        Feed feed = feedRepository.save(new Feed(requestDto, user));
+        return new FeedResponseDto(feed);
+    }
+
+    //피드 전체 조회
+    @Override
+    public List<FeedResponseDto> selectFeeds() {
+        return feedRepository.findAll().stream().map(FeedResponseDto::new).toList();
+    }
+
+    //피드 상세 조회
+    @Override
+    public Feed selectFeed(Long id) {
+        return feedRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 피드가 존재하지 않습니다."));
+    }
+
+    //피드 수정
+    @Override
+    public FeedResponseDto updateFeed(Long id, FeedRequestDto requestDto, User user) {
+        String username = selectFeed(id).getUser().getUsername();
+        Feed feed = selectFeed(id);
+        if (!(user.getRole().equals(UserRoleEnum.ADMIN) || username.equals(user.getUsername()))) {
+            throw new RejectedExecutionException();
+        }
+        feed.setTitle(requestDto.getTitle());
+        feed.setContent(requestDto.getContent());
+        return new FeedResponseDto(feed);
+    }
+
+    //피드 삭제
+    @Override
+    public void deleteFeed(Long id, User user) {
+        String username = selectFeed(id).getUser().getUsername();
+        Feed feed = selectFeed(id);
+        if (!(user.getRole().equals(UserRoleEnum.ADMIN) || username.equals(user.getUsername()))) {
+            throw new RejectedExecutionException();
+        } else feedRepository.delete(feed);
+    }
+
+
 }
