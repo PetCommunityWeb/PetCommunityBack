1664228b5ec2e23aea6ffa041839f6856bec97f6
Jongwoo Choi <muvnelik@naver.com>
2023-08-21 17:12:31 +0900

    feat: Feed Comment 기능 구현
    - 코멘트 POST method , controller 완성

--- src/main/java/com/example/petback/comment/controller/CommentController.java
+++ src/main/java/com/example/petback/comment/controller/CommentController.java
@@ -3,10 +3,16 @@ package com.example.petback.comment.controller;
 import com.example.petback.comment.dto.CommentRequestDto;
 import com.example.petback.comment.dto.CommentResponseDto;
 import com.example.petback.comment.service.CommentService;
+import com.example.petback.common.advice.ApiResponseDto;
 import com.example.petback.common.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.RejectedExecutionException;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +28,28 @@ public class CommentController {
         return commentService.createComment(commentRequestDto, userDetails.getUser(),id);
     }
 
-//    // 코멘트 조회
-//
-//    @GetMapping("/")
-//    public
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponseDto> updateComment(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                        @PathVariable Long id,
+                                                        @RequestBody CommentRequestDto requestDto) {
+        try {
+            CommentResponseDto result = commentService.updateComment(id, requestDto, userDetails.getUser());
+            return ResponseEntity.ok().body(new ApiResponseDto("수정이 완료되었습니다", HttpStatus.OK.value()));
+        } catch (RejectedExecutionException e) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto(("작성자만 수정할 수 있습니다", HttpStatus.BAD_REQUEST.value())));
+
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponseDto> deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails,
+                                                        @PathVariable Long id){
+        try{
+            commentService.deleteComment(id, userDetails.getUser());
+            return ResponseEntity.ok().body(new ApiResponseDto("삭제 되었습니다." , HttpStatus.OK.value()));
+        } catch (RejectedExecutionException c) {
+            return ResponseEntity.badRequest().body(new ApiResponseDto("작성자만 삭제할 수 있습니다.", HttpStatus.BAD_REQUEST.value()));
+        }
+    }
 }
