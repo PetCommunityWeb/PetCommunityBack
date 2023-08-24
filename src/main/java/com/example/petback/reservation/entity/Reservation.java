b07b39e1f103e5b69e051707a20b633cfea12929
hyukjin <cdh5771@naver.com>
2023-08-24 10:30:17 +0900

    refactor: reservationSlot 지연 로딩으로 수정

--- src/main/java/com/example/petback/reservation/entity/Reservation.java
+++ src/main/java/com/example/petback/reservation/entity/Reservation.java
@@ -28,7 +28,7 @@ public class Reservation {
     private User user;
     @ManyToOne
     private Hospital hospital;
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     private ReservationSlot reservationSlot;
 
     public void cancle() {
