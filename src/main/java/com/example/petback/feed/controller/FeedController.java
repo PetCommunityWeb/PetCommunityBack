7e8c0f916f34a63e2ab1b51ea6658ae3e3463070
hyukjin <cdh5771@naver.com>
2023-08-24 23:08:52 +0900

    refactor: feed 컨트롤러 수정

--- src/main/java/com/example/petback/feed/controller/FeedController.java
+++ src/main/java/com/example/petback/feed/controller/FeedController.java
@@ -5,8 +5,6 @@ import com.example.petback.common.security.UserDetailsImpl;
 import com.example.petback.feed.dto.FeedRequestDto;
 import com.example.petback.feed.dto.FeedResponseDto;
 import com.example.petback.feed.service.FeedService;
-import com.example.petback.user.service.UserService;
-import com.sun.jdi.request.DuplicateRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,17 +19,17 @@ import java.util.concurrent.RejectedExecutionException;
 @RequestMapping("/api/feeds")
 public class FeedController {
     private final FeedService feedService;
-    private final UserService userService;
 
     //피드 생성
-    @PostMapping("/")
-    public FeedResponseDto createFeed(@RequestBody FeedRequestDto requestDto,
+    @PostMapping
+    public ResponseEntity createFeed(@RequestBody FeedRequestDto requestDto,
                                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        return feedService.createFeed(requestDto, userDetails.getUser());
+        FeedResponseDto responseDto = feedService.createFeed(requestDto, userDetails.getUser());
+        return ResponseEntity.ok().body(responseDto);
     }
 
     //피드 전체 조회
-    @GetMapping("/")
+    @GetMapping
     public ResponseEntity selectFeeds() {
         List<FeedResponseDto> responseDtos = feedService.selectFeeds();
         return ResponseEntity.ok().body(responseDtos);
@@ -57,9 +55,8 @@ public class FeedController {
         }
 
     }
-    //피드 삭제
-    // softDelete 함 (Feed Entity 참고)
 
+    //피드 삭제
     @DeleteMapping("/{id}")
     public ResponseEntity<ApiResponseDto> deleteFeed(@AuthenticationPrincipal UserDetailsImpl userDetails,
                                                      @PathVariable Long id) {
@@ -73,14 +70,9 @@ public class FeedController {
 
     // 피드 좋아요
     @PostMapping("/{id}/likes")
-    public ResponseEntity<ApiResponseDto> likeFeed(@AuthenticationPrincipal UserDetailsImpl userDetails,
-                                                   @PathVariable Long id){
-        try{
-            feedService.likeFeed(id, userDetails.getUser());
-        }catch (DuplicateRequestException e) {
-            return ResponseEntity.badRequest().body(new ApiResponseDto(e.getMessage(),HttpStatus.BAD_REQUEST.value()));
-        }
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponseDto("게시글 좋아요 성공",HttpStatus.ACCEPTED.value()));
+    public ResponseEntity likeFeed(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id){
+        feedService.likeFeed(id, userDetails.getUser());
+        return ResponseEntity.ok().body(new ApiResponseDto("게시글 좋아요 성공", HttpStatus.OK.value()));
     }
 
     @DeleteMapping("/{id}/likes")
