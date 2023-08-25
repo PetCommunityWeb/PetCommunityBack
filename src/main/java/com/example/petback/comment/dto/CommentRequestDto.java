e9734dba11f2196e80185af157c80bbd8e248f4d
hyukjin <cdh5771@naver.com>
2023-08-25 12:18:33 +0900

    refactor: 댓글 request 수정

--- src/main/java/com/example/petback/comment/dto/CommentRequestDto.java
+++ src/main/java/com/example/petback/comment/dto/CommentRequestDto.java
@@ -1,17 +1,20 @@
 package com.example.petback.comment.dto;
 
 import com.example.petback.comment.entity.Comment;
+import com.example.petback.feed.entity.Feed;
+import com.example.petback.user.entity.User;
 import lombok.Getter;
 
 @Getter
 public class CommentRequestDto {
 
-    private Long id;
     private String content;
 
-    public Comment toEntity() {
+    public Comment toEntity(Feed feed, User user) {
         return Comment.builder()
                 .content(content)
+                .feed(feed)
+                .user(user)
                 .build();
     }
 }
