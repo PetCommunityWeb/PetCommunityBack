5b12889476aa5564cc526bf08ad7c9030d3db7d5
SongIsak <thddltkr10@naver.com>
2023-09-05 12:38:34 +0900

    refactor: refreshToken 만료시 exception handling

--- src/main/java/com/example/petback/common/security/JwtAuthorizationFilter.java
+++ src/main/java/com/example/petback/common/security/JwtAuthorizationFilter.java
@@ -20,6 +20,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Slf4j(topic = "JWT 검증 및 인가")
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
@@ -47,10 +48,17 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                     throw new IllegalArgumentException("유효하지 않은 JWT 입니다."); // 이것을 ExpiredJwtException 보다 늦게 하는 문제
                 }
             } catch (ExpiredJwtException e) { // 다른 jwt Exception이 아닌 만료된 경우에만 재발급하기 위해, trouble: 다른 토큰 값을 임의로 넣어도 되는문제
-                Map<String, String> tokens = userService.refreshToken(refreshToken);
-                token = tokens.get("accessToken");
-                response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token); // header에 담아주기
-                token = token.substring(7); // getUserInfo를 위해 substring
+                try {
+                    Map<String, String> tokens = userService.refreshToken(refreshToken);
+                    token = tokens.get("accessToken");
+                    response.addHeader(JwtUtil.AUTHORIZATION_HEADER, token); // header에 담아주기
+                    token = token.substring(7); // getUserInfo를 위해 substring
+                } catch (NoSuchElementException nse) {
+                    response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403 Forbidden 상태 설정
+                    response.getWriter().write("refreshToken is expired"); // 에러 메시지 설정
+                    response.getWriter().flush();
+                    return; // 여기서 리턴하여 필터 체인 종료
+                }
             }
             Claims info = jwtUtil.getUserInfoFromToken(token);
             setAuthentication(info.getSubject());
