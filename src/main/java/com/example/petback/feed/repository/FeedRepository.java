9c63f7f8ac8d06b5a529569e73428bc319373645
hyukjin <cdh5771@naver.com>
2023-09-10 23:46:55 +0900

    feat: 피드 좋아요 순 조회 추가 및 캐싱처리

--- src/main/java/com/example/petback/feed/repository/FeedRepository.java
+++ src/main/java/com/example/petback/feed/repository/FeedRepository.java
@@ -3,6 +3,7 @@ package com.example.petback.feed.repository;
 import com.example.petback.feed.entity.Feed;
 import com.example.petback.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     Optional<Feed> findByTitle(String 테스트피드제목);
     List<Feed> findByUser(User user);
+    @Query(value = "SELECT f.* FROM feeds f LEFT JOIN feed_like fl ON f.id = fl.feed_id WHERE f.is_deleted = false GROUP BY f.id ORDER BY COUNT(fl.feed_id) DESC", nativeQuery = true)
+    List<Feed> findAllOrderByLikes();
 }
