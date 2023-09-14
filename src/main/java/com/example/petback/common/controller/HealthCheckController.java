c86aa9498230f2eed29fb9af4d59903265a74fe4
hyukjin <cdh5771@naver.com>
2023-09-14 19:58:18 +0900

    fix: sout을 log로 변경

--- src/main/java/com/example/petback/common/controller/HealthCheckController.java
+++ src/main/java/com/example/petback/common/controller/HealthCheckController.java
@@ -1,15 +1,17 @@
 package com.example.petback.common.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HealthCheckController {
     @GetMapping("/actuator/health")
     public ResponseEntity checkHealthStatus() {
-        System.out.println("정상");
+        log.info("정상");
         return ResponseEntity.ok("version2");
     }
 }
